48118caad500a174c92f5329703af1cc4d8c3c5f
root <root@localhost.localdomain>
2020-04-26 19:49:09 +0530

    get all data in json file

--- livecoin/spiders/coin.py
+++ livecoin/spiders/coin.py
@@ -8,22 +8,27 @@ class CoinSpider(scrapy.Spider):
     allowed_domains = ['www.livecoin.net/en']
 
     script = '''
-            function main(splash, args)
-                splash.private_mode_enabled = false
-                url = args.url
-                assert(splash:go(url))
-                assert(splash:wait(1))
-                usd_tab = assert(splash:select_all(".filterPanelItem___2z5Gb"))
-                usd_tab[3]:mouse_click()
-                assert(splash:wait(1))
-                splash:set_viewport_full()
-                return splash:png()
-            end
+        function main(splash, args)
+            splash.private_mode_enabled = false
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(1))
+            usd_tab = assert(splash:select_all(".filterPanelItem___2z5Gb"))
+            usd_tab[3]:mouse_click()
+            assert(splash:wait(1))
+            splash:set_viewport_full()
+            return splash:html()
+        end
     '''
     def start_requests(self):
-        yield SplashRequest(url='http://www.livecoin.net/en/', callback=self.parse,endpoint="execute",args={
+        yield SplashRequest(url='https://www.livecoin.net/en/', callback=self.parse,endpoint="execute",args={
             'lua_source':self.script
         })
 
     def parse(self, response):
-        print(response.body)
+        # print(response.body)
+        for currency in response.xpath("//div[contains(@class,'ReactVirtualized__Table__row tableRow___3EtiS ')]"):
+            yield{
+                'currency_pair':currency.xpath(".//div[1]/div/text()").get(),
+                'volume(24h)':currency.xpath(".//div[2]/span/text()").get()
+            }
